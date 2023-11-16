296f7196e182cc3520cc5e08957568ed91c882ac
mlevitorresv <levitorres66@gmail.com>
2023-11-16 17:22:04 +0100

    FOOTER

--- js/index.js
+++ js/index.js
@@ -2,36 +2,16 @@ const burgerButton = document.getElementById('burgerIcon');
 const burgerMenu = document.getElementById('menu');
 let isOpen = false;
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
 
-  const handleMenu = () => {
-    if(!isOpen){
-      burgerMenu.style.display = "block"
-      isOpen = true;
-    }else{
-      burgerMenu.style.display = "none"
-      isOpen = false;
-    }
+
+const handleMenu = () => {
+  if(!isOpen){
+    burgerMenu.style.display = "block"
+    isOpen = true;
+  }else{
+    burgerMenu.style.display = "none"
+    isOpen = false;
   }
+}
 
-  burgerButton.onclick = handleMenu;
+burgerButton.onclick = handleMenu;
