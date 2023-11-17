2b6f8904ca9f52c67b1d9ac72383267a3f00983b
mlevitorresv <levitorres66@gmail.com>
2023-11-17 12:04:21 +0100

    FIX: MENU

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const burgerButton = document.getElementById('burgerIcon');
+const crossBurgerButton = document.getElementById('crossBurgerIcon');
 const burgerMenu = document.getElementById('menu');
 let isOpen = false;
 
@@ -6,10 +7,14 @@ let isOpen = false;
 
 const handleMenu = () => {
   if(!isOpen){
-    burgerMenu.style.display = "block"
+    burgerMenu.style.display = "block";
+    // burgerButton.style.display = 'none';
+    // crossBurgerButton.style.display = 'block';
     isOpen = true;
   }else{
     burgerMenu.style.display = "none"
+    burgerButton.style.display = 'block';
+    // crossBurgerButton.style.display = 'none';
     isOpen = false;
   }
 }
